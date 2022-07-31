5956db006ab2aebc775bf5c03c70757fc3221a8e
Luiz-de-Lima <luiz12359@gmail.com>
2022-07-31 12:59:39 -0300

    gerador de conselhos versão 1.0

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { Footer } from "./components/Footer";
+import dividerDesktop from "./assets/pattern-divider-desktop.svg";
+import iconDice from "./assets/icon-dice.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [text, setText] = useState([]);
+  const url = "https://api.adviceslip.com/advice";
+
+  const handleAdvice = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setText(data.slip);
+  };
+  useEffect(() => {
+    handleAdvice();
+  }, []);
+
+  return (
+    <main className="App">
+      <div className="app-device">
+        <>
+          <h5>Advice #{text.id}</h5>
+          <p>{text.advice}</p>
+        </>
+
+        <img src={dividerDesktop} alt="divider desktop" />
+      </div>
+      <div className="App__icon--image">
+        <img src={iconDice} alt="icon-dice" onClick={handleAdvice} />
+      </div>
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer className="attribution">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Luiz de Lima</a>.
+    </footer>
+  );
+};
